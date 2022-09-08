a898f18dbc65044fc41f6cd242901bdd1c2fdedc
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 <ikegwimik@gmail.com>
2022-09-08 10:21:47 -0700

    update file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,2 @@
 #!/bin/bash
-int main (){char Mystr[] = "Programming is like building a multilingual puzzle, \n"; puts(Mystr); return 0;
+int main (){char Mystr[] = "Programming is like building a multilingual puzzle, \n"; puts(Mystr); return 0;};  gcc 4-puts.c ./a.out
