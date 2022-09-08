e8ff152ec7f93a14a98395c99978a86f054270f2
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 <ikegwimik@gmail.com>
2022-09-08 11:25:20 -0700

    update file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
-
+/**
+ *  * main - main block
+ *  *Description: "testing"
+ *   * Return: 0
+ *
+ **/
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+		puts("\"Programming is like building a multilingual puzzle");
 			return (0);
 }
