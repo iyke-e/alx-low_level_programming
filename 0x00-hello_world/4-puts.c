72607db5a3d650d9e2aeb406b767267a41dd2653
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 <ikegwimik@gmail.com>
2022-09-08 10:55:54 -0700

    update file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,2 @@
 #!/bin/bash
-#include <studio.h> int main (void){puts("\"Programming is like building a multilingual puzzle, \n"); return (0);};
+echo '#include <studio.h> int main (void){puts("\"Programming is like building a multilingual puzzle, \n"); return (0);}' > 4-puts.c; gcc 4-puts.c &&  ./a.out 
