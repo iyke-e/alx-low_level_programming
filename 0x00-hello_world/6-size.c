636a1d6beb3fd404b2d719ff2a02ba05ceea3f9a
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 <ikegwimik@gmail.com>
2022-09-08 12:08:54 -0700

    update file

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,6 +9,8 @@ int main(void)
 	printf("%lu\n", sizeof(char));
 	printf("%lu\n", sizeof(int));
 	printf("%lu\n", sizeof(float));
+	printf("%lu\n", sizeof(long));
+	printf("%lu\n", sizeof(long double));
 	printf("%lu\n", sizeof(double));
 	return (0);
 }
