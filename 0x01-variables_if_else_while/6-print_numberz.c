e42d3727b7d01aafb857448e305faeb974cba19b
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 <ikegwimik@gmail.com>
2022-09-09 11:57:11 -0700

    project update

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio>
+/**
+ * main - main block
+ * Descrition: this code should print numbers
+ * Return: Always 0
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+		putchar((num % 10) + '0';
+	putchar('\n');
+	return (0);
+}
