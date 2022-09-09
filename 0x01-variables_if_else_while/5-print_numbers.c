7b735380f3ef02bac75b62379206e73014689854
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 <ikegwimik@gmail.com>
2022-09-09 11:53:25 -0700

    project update

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main -main block
+ * Description: printing Numbers
+ * Return: Always 0
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+		printf("%i", num);
+	printf("\n");
+		return (0);
+}
