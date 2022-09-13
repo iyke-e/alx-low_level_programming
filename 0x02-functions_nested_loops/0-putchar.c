7c3a2fce63de3d7cbb789bd8ae996aba87e01e2e
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 <ikegwimik@gmail.com>
2022-09-13 12:00:03 -0700

    update project solution

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - main code block
+ * Description: program should print _putchar followed by a new line
+ * Return: Always 0
+ */
+int main(void)
+{
+	printf("_putchar\n")
+		return (0)
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+solution to the function and nested loops
