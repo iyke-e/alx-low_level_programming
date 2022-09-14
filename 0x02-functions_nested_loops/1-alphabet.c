657791314bb282fc50b757aa60f2496f00e89838
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 <ikegwimik@gmail.com>
2022-09-13 17:43:48 -0700

    update project solution

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,13 +4,13 @@
  * Description: Print alphabet in lowercase
  * Return: Always 0
  */
-void print_alphabet(void);
+void print_alphabet(void)
 {
-	char letter = 'a';	
-	while (letter <= 'z')
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
 	{
-		putchar(letter);
-		letter++
+		putchar(c);
 	}
 	putchar('\n');
 }
