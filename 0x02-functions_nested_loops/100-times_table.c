2ac54d0be4f35cf087bc7256cf8601e7cb0b242b
IYKE <106726596+iyke-e@users.noreply.github.com>
2022-09-14 12:22:04 +0100

    Update 100-times_table.c
    explanation to the code

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -6,44 +6,44 @@
  */
 void print_times_table(int n)
 {
-	int x, y, z;
+	int x, y, z; // x = num1, y = num 2, z = product of the x and y
 
-	if (n >= 0 && n <= 14)
+	if (n >= 0 && n <= 14) // if n is less than or equal to zero and n is greater than or equal to 14
 	{
-		for (x = 0; x <= n; x++)
+		for (x = 0; x <= n; x++) //run the possible values of n till n itself
 		{
-			for (y = 0; y <= n; y++)
+			for (y = 0; y <= n; y++) // run the possible value for y till y itself
 			{
-				z = x * y;
-				if (z > 99)
+				z = x * y; // multiply x and y 
+				if (z > 99) // if the product of x and y is greater than 99 i.e 3 digits
 				{
-					_putchar(',');
-					_putchar(32);
-					_putchar((z / 100) + '0');
-					_putchar(((z / 10) % 10) + '0');
-					_putchar((z % 10) + '0');
+					_putchar(','); //add comman
+					_putchar(32); // add space
+					_putchar((z / 100) + '0'); //get the first digit of the product
+					_putchar(((z / 10) % 10) + '0'); //get the second digit of the product
+					_putchar((z % 10) + '0'); //get the last digit of the product
 				}
-				else if (z > 9)
+				else if (z > 9) // if the product of x and y is greater than 9 i.e two digit
 				{
-					_putchar(',');
-					_putchar(32);
-					_putchar(32);
-					_putchar(((z / 10) % 10) + '0');
-					_putchar((z % 10) + '0');
+					_putchar(',');  //add comma
+					_putchar(32); //add space
+					_putchar(32); //add space
+					_putchar(((z / 10) % 10) + '0'); //get first digit 
+					_putchar((z % 10) + '0'); // get the second digit
 				}
 				else
 				{
-					if (y != 0)
+					if (y != 0) //if the second num 7 is not 0
 					{
-						_putchar(',');
-						_putchar(32);
-						_putchar(32);
-						_putchar(32);
+						_putchar(','); //add comma
+						_putchar(32); //add space
+						_putchar(32); //add space
+						_putchar(32); //add space
 					}
-					_putchar(z + '0');
+					_putchar(z + '0'); //or just write the product
 				}
 			}
-			_putchar('\n');
+			_putchar('\n'); // and add a new line
 		}
 	}
 }
