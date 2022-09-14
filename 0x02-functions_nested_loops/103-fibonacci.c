ad58b57e82391a7a75a442e8808f78be07f0a854
IYKE <106726596+iyke-e@users.noreply.github.com>
2022-09-14 13:23:30 +0100

    Update 103-fibonacci.c
    update explanation

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -7,16 +7,16 @@
 int main(void)
 {
 	int i = 0;
-	long j = 1, k = 2, sum = k;
+	long j = 1, k = 2, sum = k; //j = previous sum and k = current sum; j =1 and k =2 is our starting points
 
-	while (k + j < 4000000)
+	while (k + j < 4000000) //so far the sum of previous and current is less than 400000000
 	{
-		k += j;
-		if (k % 2 == 0)
-			sum += k;
-		j = k - j;
-		++i;
+		k += j; // current sum = sum + previous sum
+		if (k % 2 == 0) // if the sum is an even num 
+			sum += k; // sum would be increase by the current sum
+		j = k - j; // previous sum = current sum - previous sum
+		++i; //keep increaseing the number value by 1 till condition above is met
 	}
-	printf("%ld\n", sum);
+	printf("%ld\n", sum); //finally print the sum of all the sums in the sequence
 	return (0);
 }
