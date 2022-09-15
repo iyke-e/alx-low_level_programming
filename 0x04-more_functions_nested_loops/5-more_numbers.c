a134fd7c103ad68777320335a9887d54fca54b82
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 <ikegwimik@gmail.com>
2022-09-15 15:06:34 -0700

    more function solution updated

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,13 +9,13 @@ void more_numbers(void)
 {
 	int num, count;
 
-	for (count = 0; count < 10; count++;)
+	for (count = 0; count <= 9; count++;)
 	{
-		for (num = 0; num < 15; num++)
+		for (num = 0; num <= 14; num++)
 		{
 			if (num > 9)
 				_putchar ((num / 10) + '0');
-				_putchar((num % 10) + '0');
+			_putchar((num % 10) + '0');
 		}
 		_putchar('\n');
 	}
