89896f9450e2f45cb597b9d97d2ae7e20d245ee9
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 <ikegwimik@gmail.com>
2022-09-15 16:26:37 -0700

    more function solution updated

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * void print_diagonal - funtion that draws diagonal lines
+ * @n number of space before line
+ */
+
+void print_diagonal(int n)
+{
+	int count, space;
+
+	if (n > 0)
+	{
+		for (count = 0; count < n; count++)
+		{
+			for (space = 1; space < count; space++)
+				_putchar (' ');
+
+			_putchar('\\');
+			_putchar('\n')
+		}
+	}
+	_putchar('\n')
+}
+
+		
+
