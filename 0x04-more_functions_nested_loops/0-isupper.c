bdc149e196d9e2b71ae1413673a6a8a9adbda513
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 <ikegwimik@gmail.com>
2022-09-15 00:51:09 -0700

    more function solution updated

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isupper - Checks for uppercase characters.
+ * @c: character to be checked
+ * Return: 1 if character is uppercase, and 0 if otherwise
+ */
+
+int _isupper(int c)
+{
+	if (c >= 'A' || c <= 'Z')
+		return (1);
+	else
+		return (0)
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+this is the solution to the more functions more nested loop 
