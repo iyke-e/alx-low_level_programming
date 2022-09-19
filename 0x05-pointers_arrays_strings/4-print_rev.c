6421ed2c48e0662a9320bfd376424e77187fe9df
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 <ikegwimik@gmail.com>
2022-09-19 14:00:52 -0700

    solution update

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -7,13 +7,12 @@
 
 void print_rev(char *s)
 {
-	int len = 0;
-	int index;
+	int len = 0, index = len - 1;
 
-	while (s[index++])
+	while(s[index++])
 		len++;
 
-	for (index = len - 1; index >= 0; index--)
+	for (index; index >= 0; index--)
 		_putchar(s[index]);
 
 	_putchar('\n');
