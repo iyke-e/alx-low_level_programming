6b296b72a9051f4430a25ab775a26fc57cdda814
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 <ikegwimik@gmail.com>
2022-09-19 21:11:27 -0700

    solution update

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strcpy - copy string pointed by src and return value to dest pointer
+ * @dest: place to copy the string to
+ * @src: the source string to copy.
+ * Return: pointer to @dest.
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int index = 0;
+
+	while (src[index] != 0)
+	{
+		dest[index] = scr[index];
+		index++;
+	}
+	return (dest);
+}
