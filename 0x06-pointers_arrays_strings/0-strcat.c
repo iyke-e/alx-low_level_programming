a0a93269ba8a5ecc8b9c7ed803ede3499036b38d
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 <ikegwimik@gmail.com>
2022-09-22 15:32:42 -0700

    solution update

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _strcat - appends tow strings 
+ * @dest: sting to be added to another
+ * @src: string to be added to another
+ * Return: dest
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int i, j;
+
+	i = j = 0;
+	while (dest[i++])
+		j++;
+	for (i = 0; src[i] != 0; i++)
+		dest[j++] = src[i];
+
+	return (dest);
+}
