206dad65e8958417d4025d07baa682bc254281ec
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 <ikegwimik@gmail.com>
2022-09-23 02:29:34 -0700

    solution update

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -8,63 +8,129 @@
  */
 
 int a_to_int(char *s)
-{
-	int i = 0;
-	int size = 0;
-	int place = 1;
-	int num = 0;
 
-	while (s[i])
-	{
-		size++;
-		i++;
-	}
-	for (i = (size - 1); i >= 0; i--)
-	{
-		num += ((s[i] - '0') * place);
-		place *= 10;
-	}
-	return (num);
+{
+
+		int i = 0;
+
+			int size = 0;
+
+				int place = 1;
+
+					int num = 0;
+
+
+
+						while (s[i])
+
+								{
+
+											size++;
+
+													i++;
+
+														}
+
+							for (i = (size - 1); i >= 0; i--)
+
+									{
+
+												num += ((s[i] - '0') * place);
+
+														place *= 10;
+
+															}
+
+								return (num);
+
 }
 
+
+
 /**
- * infinite_add - adds two numbers
- * @n1: first input number
- * @n2: second input number
- * @r: input buffer to return
- * @size_r: size of buffer
- * Return: pointer to buffer or 0 if size of result is greater than buffer size
- */
+ *
+ *  * infinite_add - adds two numbers
+ *
+ *   * @n1: first input number
+ *
+ *    *
+ *
+ *     * @n2: second input number
+ *
+ *      *
+ *
+ *       * @r: input buffer to return
+ *
+ *        *
+ *
+ *         * @size_r: size of buffer
+ *
+ *          *
+ *
+ *           * Return: pointer to buffer or 0 if size of result is greater than buffer size
+ *
+ *            */
+
+
 
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
+
 {
-	int num1, num2, sum, n, digits, i;
 
-	num1 = a_to_int(n1);
-	num2 = a_to_int(n2);
-	sum = num1 + num2;
-	n = sum;
-	digits = 1;
-	i = 0;
+		int num1, num2, sum, n, digits, i;
 
-	while ((n / 10) != 0)
-	{
-		digits++;
-		n /= 10;
-	}
-	if ((digits + 1) > size_r)
-	{
-		return (0);
-	}
-	else
-	{
-		r[digits] = '\0';
-		for (i = (digits - 1); i >= 0; i--)
-		{
 
-			r[i] = (sum % 10) + '0';
-			sum /= 10;
-		}
-	}
-	return (r);
+
+			num1 = a_to_int(n1);
+
+				num2 = a_to_int(n2);
+
+					sum = num1 + num2;
+
+						n = sum;
+
+							digits = 1;
+
+								i = 0;
+
+
+
+									while ((n / 10) != 0)
+
+											{
+
+														digits++;
+
+																n /= 10;
+
+																	}
+
+										if ((digits + 1) > size_r)
+
+												{
+
+															return (0);
+
+																}
+
+											else
+
+													{
+
+																r[digits] = '\0';
+
+																		for (i = (digits - 1); i >= 0; i--)
+
+																					{
+
+																									r[i] = (sum % 10) + '0';
+
+																												sum /= 10;
+
+																														}
+
+																			}
+
+												return (r);
+
 }
