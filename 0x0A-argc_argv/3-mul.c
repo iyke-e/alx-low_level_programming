db1a3a2cad6d86fbe16acf90255bce04699f1a64
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 <ikegwimik@gmail.com>
2022-09-30 14:29:50 -0700

    solution updated

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - multiplies two numbers
+ * @argc: number of arguments passed to the function
+ * @argv: argument vector of pointers to strings
+ *
+eturn: 0 
