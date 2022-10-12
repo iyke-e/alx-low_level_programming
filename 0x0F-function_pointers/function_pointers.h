ed5b44b11c604f03708dc6049bea9658095fcdf3
IYKE <106726596+iyke-e@users.noreply.github.com>
2022-10-12 18:54:02 +0100

    solution update

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,2 +1,14 @@
 #ifndef FUNC_H
 #define FUNC_H
+
+/**
+ * print_name - prints a name
+ * @name: name to print
+ * @f: pointer to the printing function
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (!name || !f)
+		return;
+	f(name);
+}
