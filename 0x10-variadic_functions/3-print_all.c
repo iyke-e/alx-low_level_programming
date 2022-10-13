4808a79e9cd49a60cd2bccdea02d708b77771a28
IYKE <106726596+iyke-e@users.noreply.github.com>
2022-10-13 23:45:32 +0100

    Update 3-print_all.c

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -2,12 +2,6 @@
 #include <stdio.h>
 #include <stdarg.h>
 
-void print_char(va_list arg);
-void print_int(va_list arg);
-void print_float(va_list arg);
-void print_string(va_list arg);
-void print_all(const char * const format, ...);
-
 /**
  * print_char - Prints a char.
  * @arg: A list of arguments pointing to
@@ -72,9 +66,9 @@ void print_string(va_list arg)
  * @format: A string of characters representing the argument types.
  * @...: A variable number of arguments to be printed.
  *
- * Description: Any argument not of type char, int, float,
- *              or char * is ignored.
- *              If a string argument is NULL, (nil) is printed instead.
+ * Return: Any argument not of type char, int, float,
+ * or char * is ignored.
+ * If a string argument is NULL, (nil) is printed instead.
  */
 void print_all(const char * const format, ...)
 {
